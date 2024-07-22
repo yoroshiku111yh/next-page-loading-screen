0c8139a5fc74d67619a6e4510b9fb8eee4489ec1
Minh Vu <yoroshiku111yh@users.noreply.github.com>
2024-07-22 14:59:21 +0700

    text color black

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,7 @@ import Inner from "./Inner";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <header className=" shadow-sm shadow-white container flex flex-row flex-wrap gap-5 items-center justify-start text-base font-semibold">
+      <header className=" shadow-sm shadow-white bg-white text-black container flex flex-row flex-wrap gap-5 items-center justify-start text-base font-semibold">
         <Link href={"/"}>Home</Link>
         <Link href={"/contact"}>Contact us</Link>
       </header>
